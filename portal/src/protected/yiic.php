8a7fdce3e924aa3b560aeff3c843de7cfb95e20e
Sarah Harvey <worldwise001@gmail.com>
2011-01-03 19:09:52 +0000

    adding base yii app

---
+++ portal/src/protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../../../home/shharvey/Dropbox/Documents/website/yii-1.1.5.r2654/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
