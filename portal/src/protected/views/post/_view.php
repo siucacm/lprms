b71f5c2e3b30437dbd38083f7983afb6e13352cb
Sarah Harvey <sharvey@metaether.net>
2014-10-17 23:20:19 -0700

    Clearing out old code on rails branch

--- portal/src/protected/views/post/_view.php
+++
@@ -1,7 +0,0 @@
-<div class="view">
-	<h2><?php echo CHtml::encode($data->title); ?></h2>
-	<small><?php if ($data->author != null) echo 'Posted by '.CHtml::encode($data->author->username);?> | 
-	<?php echo CHtml::encode(date('M j, Y @ g:iA', strtotime($data->timestamp))); ?></small>
-	<hr />
-	<?php echo CHtml::encode($data->content); ?>
-</div>
--- portal/src/protected/views/user/active.php
+++
@@ -1,7 +0,0 @@
-<h1>Account Activated</h1>
-
-<div class="form">
-
-Your account is now active!
-
-</div><!-- form -->
